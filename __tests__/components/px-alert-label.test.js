44418d880b5f2afa0fb616f1aaec4403dee9dd63
Jonnie Spratley <jonnie.spratley@ge.com>
2017-09-26 10:12:48 -0700

    added more test specs

--- __tests__/components/px-alert-label.test.js
+++ __tests__/components/px-alert-label.test.js
@@ -4,13 +4,14 @@ import {shallow} from 'enzyme';
 import PxAlertLabel from '../../src/px-alert-label';
 
 describe('px-alert-label', () => {
-  test('should...', () =>{
+  test('should render correctly', () =>{
     const wrapper = shallow(
       <PxAlertLabel label='Test'/>
     );
     console.log(wrapper.debug());
-    expect(true).to.equal(true);
+
+    expect(wrapper.find('.label')).to.have.length(1);
+    expect(wrapper.contains(<span className='label__text'>Test</span>)).to.equal(true);
   });
-  expect(wrapper.find('.label')).to.have.length(1);
-  expect(wrapper.contains(<span className='label__text'>Test</span>)).to.equal(true);
+
 });
