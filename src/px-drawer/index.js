4e64b8251c28327c3b083731105c68ba58d37468
Jonnie Spratley <jonnie.spratley@ge.com>
2017-09-26 16:20:12 -0700

    update drawer

--- src/px-drawer/index.js
+++ src/px-drawer/index.js
@@ -14,25 +14,59 @@ export default class Drawer extends React.Component {
   constructor(props){
     super(props);
   }
-  //handleToggle = () => this.setState({open: !this.state.open});
-
-  //handleClose = () => this.setState({open: false});
 
   toggle(){
     console.log('toggle');
   }
+
   _open(){
     console.log('open');
   }
+
   _close(){
     console.log('close');
+  }
+  componentDidMount() {
+    this.hasUnprefixedTransform = 'transform' in document.documentElement.style;
+    if (this.hasUnprefixedTransform) {
+      this._setupTouchHandlers();
+    }
+    console.log('componentDidMount', this);
+  }
+  _setupTouchHandlers() {
+    this.touchStartX = null;
+    this.sideNavTransform = null;
+    this.sideNavContent.addEventListener('touchstart', this.onSideNavTouchStart.bind(this));
+    this.sideNavContent.addEventListener('touchmove', this.onSideNavTouchMove.bind(this));
+    this.sideNavContent.addEventListener('touchend', this.onSideNavTouchEnd.bind(this));
+  }
+
+  onSideNavTouchStart(e) {
+    e.preventDefault();
+    this.touchStartX = e.touches[0].pageX;
+  }
+
+  onSideNavTouchMove(e) {
+    e.preventDefault();
+    const newTouchX = e.touches[0].pageX;
+    this.sideNavTransform = Math.min(0, newTouchX - this.touchStartX);
+    this.sideNavContent.style.transform = 'translateX(' + this.sideNavTransform + 'px)';
+  }
+
+  onSideNavTouchEnd(e) {
+    let TOUCH_SLOP = 8 * window.devicePixelRatio * 3;
+    if (this.sideNavTransform < -TOUCH_SLOP) {
+      this._close();
+      return;
+    }
+    this._open();
   }
 
   render(){
     const {
       style,
-      className = 'drawer', //root element class
-      containerClassName, //classname of container
+      className = 'drawer',
+      containerClassName,
 
       open,
 
@@ -50,8 +84,7 @@ export default class Drawer extends React.Component {
 
 
     const baseClassName = 'px-drawer';
-
-    const drawerClasses = classnames(
+    const baseClasses = classnames(
       baseClassName,
       {[`${baseClassName}--is-docked`]: docked},
       {[`${baseClassName}--is-open`]: open}
@@ -62,17 +95,17 @@ export default class Drawer extends React.Component {
       {[`${overlayClassName}--is-open`]: open}
     );
 
-    const classes = classnames(
+    const drawerClasses = classnames(
       className,
-      { [`drawer--${align}`]: true },
+      { [`${className}--${align}`]: true },
       {[`${className}--is-open`]: open},
       {[`${className}--is-docked`]: docked},
     );
 
     return (
-      <div className={drawerClasses}>
+      <div className={baseClasses}>
         {overlay && <div id="overlay" style={overlayStyle} onClick={onOverlayClick} className={overlayClasses}></div>}
-        <div id="drawer" className={classes} style={style}>
+        <div id="drawer" className={drawerClasses} style={style} ref={(e) => {this.sideNavContent = e;}}>
           <div id="drawerContent" className="drawer__content">
             <div>{children}</div>
           </div>
