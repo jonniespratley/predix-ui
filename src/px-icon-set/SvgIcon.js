be570394f311fa5691e9e3c5d2ff8e744c19dc8d
Jonnie Spratley <jonniespratley@gmail.com>
2017-10-10 10:03:04 -0700

    fixed build

--- src/px-icon-set/SvgIcon.js
+++ src/px-icon-set/SvgIcon.js
@@ -2,14 +2,13 @@ import React from 'react';
 import classnames from 'classnames';
 export default class SvgIcon extends React.Component {
   render(){
-    const {children, classes, className, titleAccess, viewBox, ...other} = this.props;
+    const {children, classes, className, titleAccess, viewBox} = this.props;
     return (
       <svg
         className={classnames(classes, className)}
         focusable="false"
         viewBox={viewBox}
         aria-hidden={titleAccess ? 'false' : 'true'}
-        {...other}
       >
         {titleAccess ? <title>{titleAccess}</title> : null}
         {children}
