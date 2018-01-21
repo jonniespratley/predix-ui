aa81f8aa33d42e20444278f66098c3d777a1e19a
Jonnie Spratley <jonnie.spratley@ge.com>
2018-01-21 08:03:35 -0800

    updated

--- __tests__/setup.js
+++ __tests__/setup.js
@@ -1,6 +1,15 @@
 import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
+
+/*
+import { mount } from 'enzyme';
+import initStoryshots from '@storybook/addon-storyshots';
+
+initStoryshots({
+  renderer: mount
+});
+*/
 import 'core-js/es6/map';
 import 'core-js/es6/set';
 
