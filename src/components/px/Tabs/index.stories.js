600f87a664374305f929c6e02287e1f56b903947
Jonnie Spratley <jonnie.spratley@ge.com>
2018-03-23 09:25:34 -0700

    fixed issue on tabs where selected = 0 was returning false, thus not updating state

--- src/components/px/Tabs/index.stories.js
+++ src/components/px/Tabs/index.stories.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
-import { withKnobs, text, boolean, number, select } from '@storybook/addon-knobs/react';
+import { withKnobs, text, number, select } from '@storybook/addon-knobs/react';
 
 import Tabs from './';
 import Tab from './Tab';
@@ -45,4 +45,27 @@ storiesOf('Tabs', module)
         </div>
       </Tab>
     </Tabs>
-	));
+  ))
+	.add('with propForSelect', () => (
+    <Tabs selected={text('selected', 'tab2')} propForSelect='id' onChange={onChange}>
+      <Tab label="Tab 1" id='tab1'>
+        <div>
+          <p>This is the tab 1 content. </p>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+        </div>
+      </Tab>
+      <Tab label="Tab 2" id='tab2'>
+        <div>
+          <p>This is the tab 2 content. </p>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+        </div>
+      </Tab>
+      <Tab label="Tab 3" id='tab3'>
+        <div>
+          <p>This is the tab 3 content. </p>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+        </div>
+      </Tab>
+    </Tabs>
+  ))
+  ;
