b59378a14892ecc7f586d73383aa6f24a325aba9
Jonnie Spratley <jonnie.spratley@ge.com>
2018-05-22 14:11:42 -0700

    removed some styles.

--- src/components/px/Layout/LayoutItem.js
+++ src/components/px/Layout/LayoutItem.js
@@ -1,28 +1,26 @@
 import styled, { css } from 'styled-components';
 
 const LayoutItem = styled.div`
- 
-
-   padding-left: 1rem;
-    flex: 1;
-    flex-basis: 13.33333rem;
-    &:nth-last-child(-n+3):first-child,
-    &:nth-last-child(-n+3):first-child~.layout__item {
+  padding-left: 1rem;
+  flex: 1;
+  flex-basis: 13.33333rem;
+  &:nth-last-child(-n+3):first-child,
+  &:nth-last-child(-n+3):first-child~.layout__item {
     flex-basis: 26.66667rem;
   }
-    &:nth-last-child(-n+4):first-child,
-    &:nth-last-child(-n+4):first-child ~ .layout__item {
-      flex-basis: 20rem;
-    }
-    @media screen and (min-width: 64em) {
+  &:nth-last-child(-n+4):first-child,
+  &:nth-last-child(-n+4):first-child ~ .layout__item {
+    flex-basis: 20rem;
+  }
+  @media screen and (min-width: 64em) {
+    flex-basis: 0;
+  }
+  @media screen and (min-width: 64em) {
+    &:nth-last-child(-n+3):first-child,
+    &:nth-last-child(-n+3):first-child ~  .layout__item {
       flex-basis: 0;
     }
-    @media screen and (min-width: 64em) {
-      &:nth-last-child(-n+3):first-child,
-      &:nth-last-child(-n+3):first-child ~  .layout__item {
-        flex-basis: 0;
-      }
-    } 
+  }
 
   ${props => props.full && css`
     align-self: stretch;
@@ -33,7 +31,7 @@ const LayoutItem = styled.div`
   ${props => props.bottom && css`
     align-self: flex-end;
   `}
-  
+
 
 `;
 
