0f16717c75b59bce5b7d73e5e4bca7cbc4818fcb
Jonnie Spratley <jonnie.spratley@ge.com>
2019-03-28 15:18:31 -0700

    fix: updated lint

--- src/Card/Card.jsx
+++ src/Card/Card.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import styled, { css, switchProp, ifProp } from '../styled';
+import styled, { ifProp } from '../styled';
 
 import Icon from '../Icon/Icon';
 
